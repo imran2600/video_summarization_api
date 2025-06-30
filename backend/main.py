3337a6b18ca97606702652f5066c5f2fd4084e5b
imran mustafa <imranmustafa189@gmail.com>
2025-06-30 10:34:45 +0500

    backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ def read_root():
 # CORS (optional if front-end hosted separately)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],  # React frontend
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
